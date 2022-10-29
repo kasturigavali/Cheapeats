9163d57e38e0d9671f41c4bfd91003453deafee4
iamkasturi <kasturi.gavali@cgu.edu>
2022-10-28 19:23:07 -0700

    rendered html file

--- hello.py
+++ hello.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html')
     # return '<h2>Hello, World! Welcome to <sub>Cheap</sub>Eats</h2>'
+
+# @app.route()
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
