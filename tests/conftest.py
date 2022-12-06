df3cde91cbd15b7f0c4d1e4fae11102c06fc1b70
iamkasturi <kasturi.gavali@cgu.edu>
2022-12-05 22:14:52 -0800

    Added pytest testing files

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from website import create_app
+from flask.testing import FlaskClient
+
+
+
+
+@pytest.fixture(scope='module')
+def app():
+    app = create_app()
+    return app
+
---
+++ tests/test_models.py
@@ -0,0 +1,37 @@
+import pytest
+from website.models.dbmodels import Cart,CartEntries
+from website import db
+
+
+
+def test_cart_items(app):
+    testcount = None
+    with app.app_context():
+        query= 'SELECT COUNT(*) AS count FROM cart'
+        cartcount = db.engine.execute(query)
+        for row in cartcount:
+            print(row)
+            testcount = row.count
+        assert testcount == 10
+
+@pytest.mark.parametrize('url', [('/shoppingcart'), ('/static/style.css'), ('/static/img1.jpg'), ('/static/cheapeats.png')])
+def test_shoppingcart(app, url):
+    with app.test_client() as test_client:
+        test = test_client.get(url)
+    assert test.status_code == 200
+
+
+@pytest.mark.parametrize('url', [('/dashboard'), ('/static/style.css'), ('/static/img1.jpg'), ('/static/cheapeats.png')])
+def test_dashboard(app, url):
+    with app.test_client() as test_client:
+        test = test_client.get(url)
+    assert test.status_code == 200
+
+
+@pytest.mark.parametrize('url', [('/'), ('/static/style.css'), ('/static/img1.jpg'), ('/static/cheapeats.png')])
+def test_index(app, url):
+    with app.test_client() as test_client:
+        test = test_client.get(url)
+    assert test.status_code == 200
+
+
